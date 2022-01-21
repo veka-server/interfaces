b4044a0ed15ed1f1ea1d6a6bd9de263314fcb24e
Veka <veka610@gmail.com>
2022-01-21 13:43:36 +0100

    Update MigrationInterface.php

--- src/MigrationInterface.php
+++ src/MigrationInterface.php
@@ -5,7 +5,52 @@ namespace VekaServer\Interfaces;
 interface MigrationInterface
 {
 
-    public function upgrade();
-    public function downgrade();
+    /**
+     * EXECUTER EN PREMIER
+     * a utiliser pour les methode d'autocommit
+     * Create / ALTER (sans breaking change)
+     *
+     * SONT DOWNGRADE ASSOCIÉ DOIT ETRE : downgrade_nettoyage
+     */
+    public function upgrade_creation();
+
+    /**
+     * EXECUTER EN SECOND
+     * a utiliser pour les methodes supportant les transactions
+     * INSERT / UPDATE / DELETE / SELECT
+     *
+     * SONT DOWNGRADE ASSOCIÉ DOIT ETRE : downgrade_data
+     */
+    public function upgrade_data();
+
+    /**
+     * EXECUTER EN TROISIEME
+     * a utiliser pour les methode d'autocommit IRREVERSIBLE
+     * Create / DROP / TRUNCATE / ALTER (avec breaking change)
+     *
+     * SONT DOWNGRADE ASSOCIÉ DOIT ETRE : downgrade_creation
+     */
+    public function upgrade_nettoyage();
+
+    /**
+     * EXECUTR EN PREMIER
+     * a utiliser pour les methode d'autocommit
+     * Create / ALTER (sans breaking change)
+     */
+    public function downgrade_creation();
+
+    /**
+     * EXECUTER EN SECOND
+     * a utiliser pour les methodes supportant les transactions
+     * INSERT / UPDATE / DELETE / SELECT
+     */
+    public function downgrade_data();
+
+    /**
+     * EXECUTER EN TROISIEME
+     * a utiliser pour les methode d'autocommit IRREVERSIBLE
+     * Create / DROP / TRUNCATE / ALTER (avec breaking change)
+     */
+    public function downgrade_nettoyage();
 
 }
