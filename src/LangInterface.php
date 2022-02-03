acea63bc84200223bcde075a5a008695c9902a4f
Veka <veka610@gmail.com>
2022-02-03 14:07:03 +0100

    Create LangInterface.php

---
+++ src/LangInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace VekaServer\Interfaces;
+
+interface LangInterface
+{
+    public function get($key);
+
+    public function has($key): bool
+
+    public function set($key, $lang, $traduction);
+
+    public function addLang($lang);
+    /**
+     * @return string
+     */
+    public function getLang(): string
+
+    /**
+     * @param string $lang
+     */
+    public function setLang(string $lang): void
+
+}
